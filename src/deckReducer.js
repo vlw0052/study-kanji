3d7971d23394a97ef25da47ca42ab1ac90f1e4fe
vlw0052 <leon@vlw2.com>
2019-06-11 11:32:48 -0500

    Reformatting and added section to store

--- src/deckReducer.js
+++ src/deckReducer.js
@@ -1,66 +1,72 @@
-import React from 'react'
+import React from 'react';
 export const initialState = {
-    currentCard: null,
-    deck: {
-        JLPTLevel: null,
-        group: null,
-        correct: [],
-        inCorrect: [],
-        unAnswered: []
-    },
-    score: {
-        correct: 0,
-        total: 0
-    },
-    isAnswered: false,
-    isCorrectAnswer: true,
-    answerCards: [],
-    showScore: false
-}
+  section: null,
+  currentCard: null,
+  deck: {
+    JLPTLevel: null,
+    group: null,
+    correct: [],
+    inCorrect: [],
+    unAnswered: []
+  },
+  score: {
+    correct: 0,
+    total: 0
+  },
+  isAnswered: false,
+  isCorrectAnswer: true,
+  answerCards: [],
+  showScore: false
+};
 
-export const CardContext = React.createContext(initialState)
+export const CardContext = React.createContext(initialState);
 
 export function deckReducer(state, action) {
-    switch (action.type) {
-        case 'SET_DECK':
-            return {
-                ...state,
-                deck: action.payload
-            }
-        case 'SET_CURRENT_CARD':
-            return {
-                ...state,
-                currentCard: action.payload
-            }
-        case 'SET_ANSWERED_CORRECT':
-            return {
-                ...state,
-                isAnswered: true,
-                isCorrectAnswer: true
-            }
-        case 'SET_ANSWERED_INCORRECT':
-            return {
-                ...state,
-                isAnswered: true,
-                isCorrectAnswer: false
-            }
-        case 'SET_ANSWER_CARDS':
-            return {
-                ...state,
-                isAnswered: false,
-                answerCards: action.payload
-            }
-        case 'SHOW_SCORE':
-            return {
-                ...state,
-                showScore: true
-            }
-        case 'HIDE_SCORE':
-            return {
-                ...state,
-                showScore: false
-            }
-        default: 
-            return state
-    }
+  switch (action.type) {
+    case 'CHANGE_SECTION':
+      return {
+        ...state,
+        section: action.payload
+      };
+    case 'SET_DECK':
+      return {
+        ...state,
+        deck: action.payload
+      };
+    case 'SET_CURRENT_CARD':
+      return {
+        ...state,
+        currentCard: action.payload
+      };
+    case 'SET_ANSWERED_CORRECT':
+      return {
+        ...state,
+        isAnswered: true,
+        isCorrectAnswer: true
+      };
+    case 'SET_ANSWERED_INCORRECT':
+      return {
+        ...state,
+        isAnswered: true,
+        isCorrectAnswer: false
+      };
+    case 'SET_ANSWER_CARDS':
+      return {
+        ...state,
+        isAnswered: false,
+        answerCards: action.payload
+      };
+    case 'SHOW_SCORE':
+      return {
+        ...state,
+        showScore: true
+      };
+    case 'HIDE_SCORE':
+      return {
+        ...state,
+        showScore: false
+      };
+    default:
+      return state;
+  }
 }
--- src/func.js
+++ src/func.js
@@ -1,96 +1,96 @@
-
+import data from './cards/data.json';
 /**
  * Creates a new start deck
- * @param {object} deck 
+ * @param {object} deck
  */
-export const createNewStartDeck = (deck) => ({
-    JLPTLevel: deck.JLPTLevel,
-    group: deck.group,
-    correct: [],
-    inCorrect: [],
-    unAnswered: [...deck.matchCards]
-})
+export const createNewStartDeck = deck => ({
+  JLPTLevel: deck.JLPTLevel,
+  group: deck.group,
+  correct: [],
+  inCorrect: [],
+  unAnswered: [...deck.matchCards]
+});
 export const areCardsEqual = (card1, card2) => {
-    if (!card1 || !card2)
-        return false
-    return card1.english === card2.english
-}
+  if (!card1 || !card2) return false;
+  return card1.english === card2.english;
+};
 
-export const chooseRandomCard = (cards) => {
-    if (cards.length === 1) {
-        return cards[0]
-    }
-    const randIndex = Math.floor(Math.random() * 1000) % (cards.length - 1)
-    return cards[randIndex]
-}
+export const chooseRandomCard = cards => {
+  if (cards.length === 1) {
+    return cards[0];
+  }
+  const randIndex = Math.floor(Math.random() * 1000) % (cards.length - 1);
+  return cards[randIndex];
+};
 
-export const getRandomItems = (items, { numberOfItems = 9, fnException = (item) => true } = {}) => {
-    let randomItems = []
-    let chosenIndexes = []
-    let triedIndexes = []
-    if (items.length <= numberOfItems)
-        return items
-    for (let i = 0; i < numberOfItems; i++) {
-        let randIndex = Math.floor(Math.random() * 1000) % (items.length - 1)
-        if (!chosenIndexes.includes(randIndex) && fnException(items[randIndex])) {
-            chosenIndexes.push(randIndex)
-            randomItems.push(items[randIndex])
-        } else {
-            i--
-        }
-        if (triedIndexes.includes(randIndex)) {
-            triedIndexes.push(randIndex)
-        }
-        if (triedIndexes.length === items.length)
-            break
+export const getRandomItems = (items, { numberOfItems = 9, fnException = item => true } = {}) => {
+  let randomItems = [];
+  let chosenIndexes = [];
+  let triedIndexes = [];
+  if (items.length <= numberOfItems) return items;
+  for (let i = 0; i < numberOfItems; i++) {
+    let randIndex = Math.floor(Math.random() * 1000) % (items.length - 1);
+    if (!chosenIndexes.includes(randIndex) && fnException(items[randIndex])) {
+      chosenIndexes.push(randIndex);
+      randomItems.push(items[randIndex]);
+    } else {
+      i--;
     }
-    return randomItems
-}
+    if (triedIndexes.includes(randIndex)) {
+      triedIndexes.push(randIndex);
+    }
+    if (triedIndexes.length === items.length) break;
+  }
+  return randomItems;
+};
 
 export const moveCardTo = (isCorrect = true) => (deck, card) => {
-    let correct
-    let inCorrect
-    if (isCorrect) {
-        correct = [...deck.correct, card]
-        inCorrect = [...deck.inCorrect.filter(c => c.kanji !== card.kanji)]
-    } else {
-        correct = [...deck.correct]
-        inCorrect = [...deck.inCorrect.filter(c => c.kanji !== card.kanji), card]
-    }
-    const unAnswered = deck.unAnswered.filter(c => c.kanji !== card.kanji)
-    return {
-        ...deck,
-        correct,
-        inCorrect,
-        unAnswered
-    }
-}
+  let correct;
+  let inCorrect;
+  if (isCorrect) {
+    correct = [...deck.correct, card];
+    inCorrect = [...deck.inCorrect.filter(c => c.kanji !== card.kanji)];
+  } else {
+    correct = [...deck.correct];
+    inCorrect = [...deck.inCorrect.filter(c => c.kanji !== card.kanji), card];
+  }
+  const unAnswered = deck.unAnswered.filter(c => c.kanji !== card.kanji);
+  return {
+    ...deck,
+    correct,
+    inCorrect,
+    unAnswered
+  };
+};
 
 export const getPercentage = (numerator, denominator) => {
-    if (denominator <= 0 || numerator <= 0) {
-        return 0
-    }
-    if (denominator === numerator)
-        return 100
-    return ((numerator / denominator) * 100).toFixed(2)
-}
+  if (denominator <= 0 || numerator <= 0) {
+    return 0;
+  }
+  if (denominator === numerator) return 100;
+  return ((numerator / denominator) * 100).toFixed(2);
+};
 
 export const getNewAnswerCards = (deck, currentCard) => {
-    return shuffle([
-        ...getRandomItems(
-            [...deck.unAnswered, ...deck.inCorrect, ...deck.correct],
-            {
-                fnException: (card) => { return !areCardsEqual(card, currentCard) },
-                numberOfItems: 8
-            }),
-        currentCard
-    ])
+  return shuffle([
+    ...getRandomItems([...deck.unAnswered, ...deck.inCorrect, ...deck.correct], {
+      fnException: card => {
+        return !areCardsEqual(card, currentCard);
+      },
+      numberOfItems: +process.env.REACT_APP_NUMBER_OF_ANSWERS || 8
+    }),
+    currentCard
+  ]);
+};
+
+export function fetchSectionDeck(section) {
+  return Promise.resolve(data);
 }
 
 export function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
